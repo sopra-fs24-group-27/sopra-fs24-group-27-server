39c997a0740863c69ac4fc1e5390ce73401162eb
Yuying8777 <clairezhong157@gmail.com>
2024-04-26 22:15:01 +0200

    Add updateUser tests to UserServiceTest&IntegrationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs24.service;
 import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -106,4 +108,25 @@ public class UserServiceIntegrationTest {
       assertNotNull(loggedOutUser);
       assertEquals(UserStatus.OFFLINE, loggedOutUser.getStatus());
   }
+
+  @Test
+public void updateUserDetails_validInputs_success() {
+
+    User testUser = new User();
+    testUser.setUsername("testUsername");
+    testUser.setPassword("password");
+    testUser.setBirthDate(null);
+    testUser.setStatus(UserStatus.ONLINE);
+    userRepository.save(testUser);
+
+    UserPostDTO userPostDTO = new UserPostDTO();
+    userPostDTO.setUsername("updatedUsername");
+    userPostDTO.setName("updatedName");
+
+    User updatedUser = userService.updateUserDetails(testUser.getId(), userPostDTO);
+
+    assertEquals(userPostDTO.getUsername(), updatedUser.getUsername());
+    assertEquals(userPostDTO.getName(), updatedUser.getName());
+  }
+
 }
